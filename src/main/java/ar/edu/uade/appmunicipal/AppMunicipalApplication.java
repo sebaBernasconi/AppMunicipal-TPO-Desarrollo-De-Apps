3b9acc584bd2b6227ba474526d725694562c54c0
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-05-18 16:41:34 -0300

    Commit inical. Proyecto de Springboot con sus dependencias creadas

---
+++ src/main/java/ar/edu/uade/appmunicipal/AppMunicipalApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.uade.appmunicipal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMunicipalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppMunicipalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ar/edu/uade/appmunicipal/AppMunicipalApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.uade.appmunicipal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMunicipalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
