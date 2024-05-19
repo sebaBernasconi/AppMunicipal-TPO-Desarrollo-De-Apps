c4310ec8ec3b8a483288d8499554032f9fd1689d
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-05-19 15:38:12 -0300

    Brrio creado con las anotaciones de springboot, constructor, getters y setters y toString

---
+++ src/main/java/ar/edu/uade/appmunicipal/model/Barrio.java
@@ -0,0 +1,55 @@
+package ar.edu.uade.appmunicipal.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "barrio")
+public class Barrio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int idBarrio;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    //Constructor
+    public Barrio(int idBarrio, String nombre) {
+        this.idBarrio = idBarrio;
+        this.nombre = nombre;
+    }
+
+    //Constructor vacio para la persistencia
+
+    public Barrio() {
+    }
+
+    //Getters y Setters
+
+    public int getIdBarrio() {
+        return idBarrio;
+    }
+
+    public void setIdBarrio(int idBarrio) {
+        this.idBarrio = idBarrio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    //To string
+
+    @Override
+    public String toString() {
+        return "Barrio{" +
+                "idBarrio=" + idBarrio +
+                ", nombre='" + nombre + '\'' +
+                '}';
+    }
+}
