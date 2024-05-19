490938c0781e5b994da28481304358638c194789
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-05-19 15:54:25 -0300

    Rubro creado con las anotaciones de springboot, constructor, getters y setters y toString

---
+++ src/main/java/ar/edu/uade/appmunicipal/model/Rubro.java
@@ -0,0 +1,56 @@
+package ar.edu.uade.appmunicipal.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "rubros")
+public class Rubro {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_rubro")
+    private int idRubro;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    //Constructor
+
+    public Rubro(int idRubro, String descripcion) {
+        this.idRubro = idRubro;
+        this.descripcion = descripcion;
+    }
+
+    //Constructor vacio para la persistencia
+
+    public Rubro() {
+    }
+
+    //Getters y Setters
+
+    public int getIdRubro() {
+        return idRubro;
+    }
+
+    public void setIdRubro(int idRubro) {
+        this.idRubro = idRubro;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    //ToString
+
+    @Override
+    public String toString() {
+        return "Rubro{" +
+                "idRubro=" + idRubro +
+                ", descripcion='" + descripcion + '\'' +
+                '}';
+    }
+}
