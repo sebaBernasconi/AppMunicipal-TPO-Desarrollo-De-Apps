492e08e2e09d38646808fd4556b8792fff322334
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-05-22 20:10:59 -0300

    Metodos para listar movimientos de reclamos por personal municipal/reclamo/fecha de movimiento en MovimientoReclamoRepository

--- src/main/java/ar/edu/uade/appmunicipal/repository/MovimientoReclamoRepository.java
+++ src/main/java/ar/edu/uade/appmunicipal/repository/MovimientoReclamoRepository.java
@@ -4,6 +4,14 @@ import ar.edu.uade.appmunicipal.model.MovimientoReclamo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Date;
+import java.util.List;
+
 @Repository
 public interface MovimientoReclamoRepository extends JpaRepository<MovimientoReclamo,Integer> {
+
+    List<MovimientoReclamo>findMovimientoReclamoByPersonalMunicipal(int legajo);
+    List<MovimientoReclamo>findMovimientoReclamoByReclamo(int idReclamo);
+    List<MovimientoReclamo>findMovimientoReclamoByFechaMovimiento(Date fecha);
+
 }
