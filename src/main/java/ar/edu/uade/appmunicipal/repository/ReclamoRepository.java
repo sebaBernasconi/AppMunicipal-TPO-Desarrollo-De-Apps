703f145ac6823569f5419e0c903bf8293f14c035
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-05-22 20:02:59 -0300

    Metodos para buscar reclamos por vecino/empleado municipal/sitio/desperfecto/estado agregados al ReclamoRepository

--- src/main/java/ar/edu/uade/appmunicipal/repository/ReclamoRepository.java
+++ src/main/java/ar/edu/uade/appmunicipal/repository/ReclamoRepository.java
@@ -4,6 +4,15 @@ import ar.edu.uade.appmunicipal.model.Reclamo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ReclamoRepository extends JpaRepository<Reclamo,Integer> {
+
+    List<Reclamo>findReclamoByVecino(int dni);
+    List<Reclamo>findReclamoByPersonalMunicipal(int legajo);
+    List<Reclamo>findReclamoBySitio(int idSitio);
+    List<Reclamo>findReclamoByDesperfecto(int idDesperfecto);
+    List<Reclamo>findReclamoByEstado(String estado);
+
 }
