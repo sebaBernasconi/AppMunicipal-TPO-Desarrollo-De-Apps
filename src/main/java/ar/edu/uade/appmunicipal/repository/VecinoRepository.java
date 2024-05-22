6d7064172cb1a112771be6650f1b30dd29485d52
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-05-22 12:52:46 -0300

    Metodo para buscar por codigo de barrio agregado al VecinoRepository

--- src/main/java/ar/edu/uade/appmunicipal/repository/VecinoRepository.java
+++ src/main/java/ar/edu/uade/appmunicipal/repository/VecinoRepository.java
@@ -4,6 +4,9 @@ import ar.edu.uade.appmunicipal.model.Vecino;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface VecinoRepository extends JpaRepository<Vecino, Integer> {
+     List<Vecino> findAllByBarrio(int codigoBarrio);
 }
