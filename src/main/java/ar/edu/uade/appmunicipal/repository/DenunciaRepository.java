becfb594a7b91815a5e467b2a8b38c98f6bb8577
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-05-22 19:54:46 -0300

    Metodos para listar denuncias por vecino, sitio y estado agregados a DenunciaRepository

--- src/main/java/ar/edu/uade/appmunicipal/repository/DenunciaRepository.java
+++ src/main/java/ar/edu/uade/appmunicipal/repository/DenunciaRepository.java
@@ -4,6 +4,12 @@ import ar.edu.uade.appmunicipal.model.Denuncia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface DenunciaRepository extends JpaRepository<Denuncia,Integer> {
+
+    List<Denuncia>findDenunciaByVecino(int dni);
+    List<Denuncia>findDenunciaBySitio(int idSitio);
+    List<Denuncia>findDenunciaByEstado(String estado);
 }
