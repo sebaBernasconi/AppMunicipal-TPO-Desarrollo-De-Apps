8c0466dc32a22e99cd22bf78a06870c7b144131b
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-05-22 12:53:52 -0300

    VecinoService creado con los metodos para recuperar todos, recuperar todos por barrio y recuperar un vecino especifico

---
+++ src/main/java/ar/edu/uade/appmunicipal/service/VecinoService.java
@@ -0,0 +1,29 @@
+package ar.edu.uade.appmunicipal.service;
+
+import ar.edu.uade.appmunicipal.model.Vecino;
+import ar.edu.uade.appmunicipal.repository.VecinoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class VecinoService {
+
+    @Autowired
+    VecinoRepository vecinoRepository;
+
+    public List<Vecino>listarTodosLosVecinos(){
+        return vecinoRepository.findAll();
+    }
+
+    public List<Vecino>listarVecinosPorBarrio(int codigoBarrio){
+        return vecinoRepository.findAllByBarrio(codigoBarrio);
+    }
+
+    public Vecino buscarVecino(int dni){
+        Optional<Vecino>vecino =vecinoRepository.findById(dni);
+        return vecino.orElse(null);
+    }
+}
