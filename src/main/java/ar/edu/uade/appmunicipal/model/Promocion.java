1afac34996f3f6c76dd18682ff0779fa47ff1365
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-05-23 09:48:21 -0300

    Promocion creada con su constructor, getters y setters y toString

---
+++ src/main/java/ar/edu/uade/appmunicipal/model/Promocion.java
@@ -0,0 +1,70 @@
+package ar.edu.uade.appmunicipal.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "promocion")
+public class Promocion {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_promocion")
+    private Integer idPromocion;
+
+    @Column(name = "descripcion")
+    private String descripcion;
+
+    @OneToOne
+    @Column(name = "id_local")
+    private Local local;
+
+    //Constructor
+
+    public Promocion(Integer idPromocion, String descripcion, Local local) {
+        this.idPromocion = idPromocion;
+        this.descripcion = descripcion;
+        this.local = local;
+    }
+
+    //Constructor vacio para la persistencia
+
+    public Promocion() {
+    }
+
+    //Getters y Setters
+
+    public Integer getIdPromocion() {
+        return idPromocion;
+    }
+
+    public void setIdPromocion(Integer idPromocion) {
+        this.idPromocion = idPromocion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Local getLocal() {
+        return local;
+    }
+
+    public void setLocal(Local local) {
+        this.local = local;
+    }
+
+    //ToString
+
+    @Override
+    public String toString() {
+        return "Promocion{" +
+                "idPromocion=" + idPromocion +
+                ", descripcion='" + descripcion + '\'' +
+                ", local=" + local +
+                '}';
+    }
+}
