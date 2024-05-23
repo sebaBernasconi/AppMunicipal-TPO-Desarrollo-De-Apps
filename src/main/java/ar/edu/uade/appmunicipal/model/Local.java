634a9ee3f1206b50198cc36c2f784f901585f7a0
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-05-23 09:42:36 -0300

    Clase que actua como local/servicio que brinda algun vecino creada con su constructor,getters y setters y toString

---
+++ src/main/java/ar/edu/uade/appmunicipal/model/Local.java
@@ -0,0 +1,98 @@
+package ar.edu.uade.appmunicipal.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "local")
+public class Local {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_local")
+    private Integer idLocal;
+
+    @ManyToOne
+    @Column(name = "id_vecino")
+    private Vecino vecino;
+
+    @ManyToOne
+    @Column(name = "id_rubro")
+    private Rubro rubro;
+
+    @OneToOne
+    @Column(name = "id_sitio")
+    private Sitio sitio;
+
+    @Column(name = "contacto")
+    private String contacto;
+
+    //Constructor
+
+    public Local(Integer idLocal, Vecino vecino, Rubro rubro, Sitio sitio, String contacto) {
+        this.idLocal = idLocal;
+        this.vecino = vecino;
+        this.rubro = rubro;
+        this.sitio = sitio;
+        this.contacto = contacto;
+    }
+
+    //Constructor vacio para la persistencia
+
+    public Local() {
+    }
+
+    //Getters y Setters
+
+    public Integer getIdLocal() {
+        return idLocal;
+    }
+
+    public void setIdLocal(Integer idLocal) {
+        this.idLocal = idLocal;
+    }
+
+    public Vecino getVecino() {
+        return vecino;
+    }
+
+    public void setVecino(Vecino vecino) {
+        this.vecino = vecino;
+    }
+
+    public Rubro getRubro() {
+        return rubro;
+    }
+
+    public void setRubro(Rubro rubro) {
+        this.rubro = rubro;
+    }
+
+    public Sitio getSitio() {
+        return sitio;
+    }
+
+    public void setSitio(Sitio sitio) {
+        this.sitio = sitio;
+    }
+
+    public String getContacto() {
+        return contacto;
+    }
+
+    public void setContacto(String contacto) {
+        this.contacto = contacto;
+    }
+
+    //ToString
+
+    @Override
+    public String toString() {
+        return "Local{" +
+                "idLocal=" + idLocal +
+                ", vecino=" + vecino +
+                ", rubro=" + rubro +
+                ", sitio=" + sitio +
+                ", contacto='" + contacto + '\'' +
+                '}';
+    }
+}
