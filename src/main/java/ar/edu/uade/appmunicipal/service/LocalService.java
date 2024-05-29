dd69c54f6e367b1cbfdba9f5c91c6710c71db72b
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-05-29 20:06:57 -0300

    LocalService creado con listar todos, buscar uno, guardar y agregar promocion. Queda comentado modificarContacto

---
+++ src/main/java/ar/edu/uade/appmunicipal/service/LocalService.java
@@ -0,0 +1,45 @@
+package ar.edu.uade.appmunicipal.service;
+
+import ar.edu.uade.appmunicipal.model.Local;
+import ar.edu.uade.appmunicipal.model.Promocion;
+import ar.edu.uade.appmunicipal.repository.LocalRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class LocalService {
+
+    @Autowired
+    LocalRepository localRepository;
+
+    public List<Local>listarLocales(){
+        return localRepository.findAll();
+    }
+
+    public Local buscarLocal(Integer idLocal){
+        Optional<Local>local = localRepository.findById(idLocal);
+        return local.orElse(null);
+    }
+
+    public Local guardarLocal(Local local){
+        return localRepository.save(local);
+    }
+
+    public Local agregarPromocion(Integer idLocal, Promocion promocion){
+        Optional<Local> local = localRepository.findById(idLocal);
+
+        local.orElse(null).setPromocion(promocion);
+        return localRepository.save(local.orElse(null));
+    }
+
+    /*public Local modificarContacto(Integer idLocal,String nuevoContacto){
+       Optional<Local> local = localRepository.findById(idLocal);
+
+        local.orElse(null).setContacto(nuevoContacto);
+        return localRepository.save(local.orElse(null));
+    }*/
+
+}
