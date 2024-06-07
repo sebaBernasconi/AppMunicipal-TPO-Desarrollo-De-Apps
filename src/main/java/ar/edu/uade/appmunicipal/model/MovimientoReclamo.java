d9196459f00e308298ea2af029386931102646ee
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-06-06 23:15:47 -0300

    Agregando anotaciones de Lombock a la clase MovimientoReclamo para codigo mas limpio

--- src/main/java/ar/edu/uade/appmunicipal/model/MovimientoReclamo.java
+++ src/main/java/ar/edu/uade/appmunicipal/model/MovimientoReclamo.java
@@ -1,11 +1,20 @@
 package ar.edu.uade.appmunicipal.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.sql.Date;
 
 @Entity
 @Table(name = "movimientos_reclamos")
+
+@Getter
+@Setter
+@AllArgsConstructor //Constructor
+@NoArgsConstructor //Constructor vacio para la persistencia
 public class MovimientoReclamo {
 
     @Id
@@ -26,64 +35,6 @@ public class MovimientoReclamo {
 
     @Column(name = "fecha_movimiento")
     private Date fechaMovimiento;
-
-    //Constructos
-
-    public MovimientoReclamo(int idMovimiento, Reclamo reclamo, PersonalMunicipal personalMunicipal,
-                             String causa, Date fechaMovimiento) {
-        this.idMovimiento = idMovimiento;
-        this.reclamo = reclamo;
-        this.personalMunicipal = personalMunicipal;
-        this.causa = causa;
-        this.fechaMovimiento = fechaMovimiento;
-    }
-
-    //Contsructor vacio para la persistencia
-
-    public MovimientoReclamo() {
-    }
-
-    //Getters y Setters
-
-    public int getIdMovimiento() {
-        return idMovimiento;
-    }
-
-    public void setIdMovimiento(int idMovimiento) {
-        this.idMovimiento = idMovimiento;
-    }
-
-    public Reclamo getReclamo() {
-        return reclamo;
-    }
-
-    public void setReclamo(Reclamo reclamo) {
-        this.reclamo = reclamo;
-    }
-
-    public PersonalMunicipal getPersonalMunicipal() {
-        return personalMunicipal;
-    }
-
-    public void setPersonalMunicipal(PersonalMunicipal personalMunicipal) {
-        this.personalMunicipal = personalMunicipal;
-    }
-
-    public String getCausa() {
-        return causa;
-    }
-
-    public void setCausa(String causa) {
-        this.causa = causa;
-    }
-
-    public Date getFechaMovimiento() {
-        return fechaMovimiento;
-    }
-
-    public void setFechaMovimiento(Date fechaMovimiento) {
-        this.fechaMovimiento = fechaMovimiento;
-    }
 
     //ToString
 
