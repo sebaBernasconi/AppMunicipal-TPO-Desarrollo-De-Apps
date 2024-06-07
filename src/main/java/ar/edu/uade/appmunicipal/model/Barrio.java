d13150c4a723da01a62ddf0c61f0273a020e36c5
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-06-06 23:11:44 -0300

    Agregando anotaciones de Lombock a la clase Barrio para codigo mas limpio

--- src/main/java/ar/edu/uade/appmunicipal/model/Barrio.java
+++ src/main/java/ar/edu/uade/appmunicipal/model/Barrio.java
@@ -1,9 +1,18 @@
 package ar.edu.uade.appmunicipal.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "barrio")
+
+@Getter
+@Setter
+@AllArgsConstructor //Constructor
+@NoArgsConstructor //Constructor vacio para persistencia
 public class Barrio {
 
     @Id
@@ -14,34 +23,6 @@ public class Barrio {
     @Column(name = "nombre")
     private String nombre;
 
-    //Constructor
-    public Barrio(int idBarrio, String nombre) {
-        this.idBarrio = idBarrio;
-        this.nombre = nombre;
-    }
-
-    //Constructor vacio para la persistencia
-
-    public Barrio() {
-    }
-
-    //Getters y Setters
-
-    public int getIdBarrio() {
-        return idBarrio;
-    }
-
-    public void setIdBarrio(int idBarrio) {
-        this.idBarrio = idBarrio;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
 
     //To string
 
