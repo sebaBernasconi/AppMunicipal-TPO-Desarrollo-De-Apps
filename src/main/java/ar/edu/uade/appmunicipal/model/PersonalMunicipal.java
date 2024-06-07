85f0bf8e29e5f481d92e8d618f4f722a491fa8b0
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-06-06 23:16:42 -0300

    Agregando anotaciones de Lombock a la clase PersonalMunicipal para codigo mas limpio

--- src/main/java/ar/edu/uade/appmunicipal/model/PersonalMunicipal.java
+++ src/main/java/ar/edu/uade/appmunicipal/model/PersonalMunicipal.java
@@ -4,11 +4,20 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.sql.Date;
 
 @Entity
 @Table(name ="personal_municipal")
+
+@Getter
+@Setter
+@AllArgsConstructor //Constructor
+@NoArgsConstructor //Constructor vacio para la persistencia
 public class PersonalMunicipal {
     @Id
     @Column(name = "legajo")
@@ -34,90 +43,6 @@ public class PersonalMunicipal {
 
     @Column(name = "fecha_ingreso")
     private Date fechaIngreso;
-
-    //Constructor
-    public PersonalMunicipal(int legajo, int dni, String nombre, String apellido,
-                             String password, int sector, int categoria, Date fechaIngreso) {
-        this.legajo = legajo;
-        this.dni = dni;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.password = password;
-        this.sector = sector;
-        this.categoria = categoria;
-        this.fechaIngreso = fechaIngreso;
-    }
-
-    //Constructor vacio para la persistencia
-
-    public PersonalMunicipal() {
-    }
-
-    //Getters y Setters
-
-    public int getLegajo() {
-        return legajo;
-    }
-
-    public void setLegajo(int legajo) {
-        this.legajo = legajo;
-    }
-
-    public int getDni() {
-        return dni;
-    }
-
-    public void setDni(int dni) {
-        this.dni = dni;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public int getSector() {
-        return sector;
-    }
-
-    public void setSector(int sector) {
-        this.sector = sector;
-    }
-
-    public int getCategoria() {
-        return categoria;
-    }
-
-    public void setCategoria(int categoria) {
-        this.categoria = categoria;
-    }
-
-    public Date getFechaIngreso() {
-        return fechaIngreso;
-    }
-
-    public void setFechaIngreso(Date fechaIngreso) {
-        this.fechaIngreso = fechaIngreso;
-    }
 
     //ToString
 
