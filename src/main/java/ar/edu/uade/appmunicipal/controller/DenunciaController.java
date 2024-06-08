72c8c1951bdb23f444e6a38f22ab5ed88e12a7d5
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-06-08 18:29:52 -0300

    DenunciaController creado. Falta agregar autorizaciones

---
+++ src/main/java/ar/edu/uade/appmunicipal/controller/DenunciaController.java
@@ -0,0 +1,78 @@
+package ar.edu.uade.appmunicipal.controller;
+
+import ar.edu.uade.appmunicipal.model.Denuncia;
+import ar.edu.uade.appmunicipal.model.Reclamo;
+import ar.edu.uade.appmunicipal.service.DenunciaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/denuncias")
+public class DenunciaController {
+
+    @Autowired
+    DenunciaService denunciaService;
+
+    public static DenunciaController instancia;
+
+    //Constructor
+
+    public DenunciaController() {
+    }
+
+    //getInstancia para singleton
+
+    public static DenunciaController getInstancia(){
+        if (instancia == null){
+            return instancia = new DenunciaController();
+        }else {
+            return instancia;
+        }
+    }
+
+    //Metodos del controller
+
+    @PostMapping(value = "/registrar",consumes = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<Denuncia>guardarDenuncia(@RequestBody Denuncia denuncia){
+        Denuncia denunciaConfirmada = denunciaService.guardarDenuncia(denuncia);
+        return new ResponseEntity<>(denunciaConfirmada, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/actualizarEstado/{id}/{estado}",produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<Denuncia>actualizarEstado(@PathVariable("id")Integer idDenuncia,
+                                                    @PathVariable("estado")String nuevoEstado){
+        try {
+            Denuncia denunciaParaActualizar = denunciaService.actualizarEstado(idDenuncia,nuevoEstado);
+            return new ResponseEntity<>(denunciaParaActualizar,HttpStatus.OK);
+        }catch (EmptyResultDataAccessException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping(value = "/listar",produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<List<Denuncia>>listarDenuncias(){
+        try {
+            List<Denuncia>listadoDeDenuncias = denunciaService.listarDenuncias();
+            return new ResponseEntity<>(listadoDeDenuncias,HttpStatus.OK);
+        }catch (EmptyResultDataAccessException e){
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }
+    }
+
+    @GetMapping(value = "buscarPorId/{id}",produces = {MediaType.APPLICATION_JSON_VALUE})
+    public ResponseEntity<Denuncia>buscarDenunciaPorId(@PathVariable("id")Integer idDenuncia){
+        try {
+            Denuncia denunciaEncontrada = denunciaService.buscarDenuncia(idDenuncia);
+            return new ResponseEntity<>(denunciaEncontrada, HttpStatus.OK);
+        }catch (EmptyResultDataAccessException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+}
