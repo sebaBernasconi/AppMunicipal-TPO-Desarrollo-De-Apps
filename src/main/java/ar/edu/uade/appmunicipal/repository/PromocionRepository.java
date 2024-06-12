0f640d8d7463e90510d950faa6a178ada69a6bf9
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-06-12 12:29:35 -0300

    Borrando todo lo referido al objeto Promocion ya que se modifico el atributo en la clase Local

--- src/main/java/ar/edu/uade/appmunicipal/repository/PromocionRepository.java
+++
@@ -1,9 +0,0 @@
-package ar.edu.uade.appmunicipal.repository;
-
-import ar.edu.uade.appmunicipal.model.Promocion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PromocionRepository extends JpaRepository<Promocion,Integer> {
-}
