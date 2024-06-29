8c56e59add8f293004add8da621424aa36bba17f
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-06-29 19:16:13 -0300

    DenunciaCard creada y DenunciasScreen consumiendo la api

--- front_end_municipio/src/screens/DenunciasScreen.jsx
+++ front_end_municipio/src/screens/DenunciasScreen.jsx
@@ -1,11 +1,54 @@
-import {StyleSheet, Text, View} from 'react-native'
-import React from 'react'
+import {FlatList, StyleSheet, Text, View} from 'react-native'
+import React, {useCallback, useState} from 'react'
+import DenunciaCard from "../components/ReclamoCard";
+import StyledScreenWrapper from "../styledComponents/StyledScreenWrapper";
+import {ipLocal} from "../global/ipLocal";
+import {useSelector} from "react-redux";
+import {useFocusEffect} from "@react-navigation/native";
+import ReclamoCard from "../components/ReclamoCard";
 
 export default function DenunciasScreen() {
+    const {jwt} = useSelector((state) => state.authReducer.value)
+    const [denuncias, setDenuncias] = useState([])
+
+    async function getDenuncias(){
+
+        try {
+            const response = await fetch(`http://${ipLocal}:8080/denuncias/listar`,{
+                method: "GET",
+                headers: {
+                    "Authorization" : `Bearer ${jwt}`
+                },
+            })
+
+            if (!response.ok){
+                throw new Error(await response.text())
+            }
+
+            const data = await response.json();
+            setDenuncias(data)
+        }catch (error){
+            console.error(error)
+        }
+
+    }
+
+    useFocusEffect(
+        useCallback(() => {
+            getDenuncias()
+        },[])
+    )
+
     return (
-        <View>
-            <Text>DenunciasScreen</Text>
-        </View>
+        <StyledScreenWrapper style={{paddingTop: 16}}>
+            <FlatList
+                data={denuncias}
+                renderItem={({item}) => (
+                    <DenunciaCard denuncia={item} navigation={navigation}/>
+                )}
+                keyExtractor={item => item.idDenuncia}
+            />
+        </StyledScreenWrapper>
     )
 }
 const styles = StyleSheet.create({})
