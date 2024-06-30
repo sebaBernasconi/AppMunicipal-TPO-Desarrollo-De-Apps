42d522dff6bdffa07e5ae5d30a6171eb204ec7f9
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-06-30 19:04:32 -0300

    Anotaciones de Swagger agregadas al DesperfectoController y RubroController

--- src/main/java/ar/edu/uade/appmunicipal/controller/DesperfectoController.java
+++ src/main/java/ar/edu/uade/appmunicipal/controller/DesperfectoController.java
@@ -3,6 +3,12 @@ package ar.edu.uade.appmunicipal.controller;
 import ar.edu.uade.appmunicipal.model.DTOs.DesperfectoDTO;
 import ar.edu.uade.appmunicipal.model.Desperfecto;
 import ar.edu.uade.appmunicipal.service.DesperfectoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
@@ -14,17 +20,35 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/desperfecto")
+@Tag(name = "Gestion de Desperfectos", description = "Endpoints de los Desperfectos")
 public class DesperfectoController {
 
     @Autowired
     DesperfectoService desperfectoService;
 
+    @Operation(summary = "Guardar Desperfecto")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "Se guardo el Desperfecto en la base de datos",content = {
+                    @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Desperfecto.class)
+                    )
+            }),
+            @ApiResponse(responseCode = "403",description = "Conflicto con el servidor",content = {@Content})
+    })
     @PostMapping(value = "/agregar",consumes = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<Desperfecto>guardarDesperfecto(@RequestBody DesperfectoDTO desperfectoDTO){
         Desperfecto desperfectoGuardado = desperfectoService.guardarDesperfecto(desperfectoDTO);
         return new ResponseEntity<>(desperfectoGuardado, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Devuelve un listado de Desperfectos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Listado de Desperfectos recuperado",content = {
+                    @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Desperfecto.class))
+            }),
+            @ApiResponse(responseCode = "403",description = "Conflicto con el servidor",content = {@Content})
+    })
     @GetMapping(value = "/listar",produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<List<Desperfecto>>listarDesperfectos(){
         try {
@@ -35,6 +59,14 @@ public class DesperfectoController {
         }
     }
 
+    @Operation(summary = "Buscar Desperfecto por ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Devuelve el Desperfecto solicitado", content = {
+                    @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Desperfecto.class))
+            }),
+            @ApiResponse(responseCode = "403", description = "Conflicto con el servidor", content = {@Content})
+    })
     @GetMapping(value = "/buscar/{id}",produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<Desperfecto>buscarDesperfecto(@PathVariable("id")Integer idDesperfecto){
         try {
--- src/main/java/ar/edu/uade/appmunicipal/controller/RubroController.java
+++ src/main/java/ar/edu/uade/appmunicipal/controller/RubroController.java
@@ -1,7 +1,14 @@
 package ar.edu.uade.appmunicipal.controller;
 
+import ar.edu.uade.appmunicipal.model.Desperfecto;
 import ar.edu.uade.appmunicipal.model.Rubro;
 import ar.edu.uade.appmunicipal.service.RubroService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
@@ -13,17 +20,35 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/rubro")
+@Tag(name = "Gestion de Rubros",description = "Endpoints de los Rubros")
 public class RubroController {
 
     @Autowired
     RubroService rubroService;
 
+    @Operation(summary = "Guardar Rubro")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "Se guardo el Rubro en la base de datos",content = {
+                    @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Rubro.class)
+                    )
+            }),
+            @ApiResponse(responseCode = "403",description = "Conflicto con el servidor",content = {@Content})
+    })
     @PostMapping(value = "/agregar",consumes = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<Rubro>guardarRubro(@RequestBody Rubro rubro){
         rubroService.guardarRubro(rubro);
         return new ResponseEntity<>(rubro, HttpStatus.CREATED);
     }
 
+    @Operation(summary = "Devuelve un listado de Rubros")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Listado de Rubros recuperado",content = {
+                    @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Rubro.class))
+            }),
+            @ApiResponse(responseCode = "403",description = "Conflicto con el servidor",content = {@Content})
+    })
     @GetMapping(value = "/listar",produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<List<Rubro>>listarRubros(){
         try {
@@ -34,6 +59,14 @@ public class RubroController {
         }
     }
 
+    @Operation(summary = "Buscar Rubro por ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Devuelve el Rubro solicitado", content = {
+                    @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Rubro.class))
+            }),
+            @ApiResponse(responseCode = "403", description = "Conflicto con el servidor", content = {@Content})
+    })
     @GetMapping(value = "/buscar/{id}",produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<Rubro>buscarRubro(@PathVariable("id")Integer idRubro){
         try {
