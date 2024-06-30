93cdd1a1715f494c1fefcd5760555bd7ab369058
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-06-30 14:09:47 -0300

    Anotaciones de Swagger agregadas a la clase Rubro

--- src/main/java/ar/edu/uade/appmunicipal/model/PersonalMunicipal.java
+++ src/main/java/ar/edu/uade/appmunicipal/model/PersonalMunicipal.java
@@ -1,5 +1,6 @@
 package ar.edu.uade.appmunicipal.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
@@ -11,6 +12,7 @@ import lombok.Setter;
 
 import java.sql.Date;
 
+@Schema(description = "PersonalMunicipal Model")
 @Entity
 @Table(name ="personal_municipal")
 
@@ -19,28 +21,37 @@ import java.sql.Date;
 @AllArgsConstructor //Constructor
 @NoArgsConstructor //Constructor vacio para la persistencia
 public class PersonalMunicipal {
+
+    @Schema(description = "Legajo del PersonalMunicipal",requiredMode = Schema.RequiredMode.REQUIRED,example = "24232")
     @Id
     @Column(name = "legajo")
     private Integer legajo;
 
+    @Schema(description = "DNI del PersonalMuinicipal",requiredMode = Schema.RequiredMode.REQUIRED,example = "22333444")
     @Column(name = "dni")
     private int dni;
 
+    @Schema(description = "Nombre del PersonalMunicipal",requiredMode = Schema.RequiredMode.REQUIRED,example = "Juan")
     @Column(name = "nombre")
     private String nombre;
 
+    @Schema(description = "Apellido del PersonalMunicipal",requiredMode = Schema.RequiredMode.REQUIRED,example = "Perez")
     @Column(name = "apellido")
     private String apellido;
 
+    @Schema(description = "Password del PersonalMunicipal",requiredMode = Schema.RequiredMode.AUTO)
     @Column(name = "password")
     private String password;
 
+    @Schema(description = "Sector donde trabaja el PersonalMunicipal",requiredMode = Schema.RequiredMode.REQUIRED,example = "Administracion")
     @Column(name = "sector")
     private String sector;
 
+    @Schema(description = "Categoria del PersonalMunicipal",requiredMode = Schema.RequiredMode.REQUIRED,example = "Categoria 2")
     @Column(name = "categoria")
     private int categoria;
 
+    @Schema(description = "Fecha de contratacion del PersonalMunicipal",requiredMode = Schema.RequiredMode.AUTO)
     @Column(name = "fecha_ingreso")
     private Date fechaIngreso;
 
