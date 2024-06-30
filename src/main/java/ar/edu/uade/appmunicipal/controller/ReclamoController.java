bd03f42956e4638a00aa16e4ae1c533d09c5d666
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-06-30 18:45:16 -0300

    Anotaciones de Swagger agregadas al ReclamoController

--- src/main/java/ar/edu/uade/appmunicipal/controller/ReclamoController.java
+++ src/main/java/ar/edu/uade/appmunicipal/controller/ReclamoController.java
@@ -4,6 +4,12 @@ import ar.edu.uade.appmunicipal.model.DTOs.ReclamoDTO;
 import ar.edu.uade.appmunicipal.model.PersonalMunicipal;
 import ar.edu.uade.appmunicipal.model.Reclamo;
 import ar.edu.uade.appmunicipal.service.ReclamoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
@@ -16,12 +22,20 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/reclamos")
-
+@Tag(name = "Gestion de Reclamos",description = "Endpoints de los Reclamos")
 public class ReclamoController {
     
     @Autowired
     ReclamoService reclamoService;
 
+    @Operation(summary = "Guardar Reclamo")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201",description = "Se guardo el Reclamo en la base de datos",content = {
+                    @Content(mediaType = "MULTIPART_FORM_DATA_VALUE",
+                    schema = @Schema(implementation = Reclamo.class))
+            }),
+            @ApiResponse(responseCode = "403",description = "Conflicto con el servidor",content = {@Content})
+    })
     @PostMapping(value = "/registrar", consumes = {MediaType.MULTIPART_FORM_DATA_VALUE})
     public ResponseEntity<?>guardarReclamo(@RequestPart ReclamoDTO reclamoDTO, @RequestParam MultipartFile archivo){
         try {
@@ -31,6 +45,14 @@ public class ReclamoController {
         }
     }
 
+    @Operation(summary = "Actualizar estado del Reclamo por ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Se actualizo el estado del Reclamo",content = {
+                    @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Reclamo.class))
+            }),
+            @ApiResponse(responseCode = "403",description = "Conflicto con el servidor",content = {@Content})
+    })
     @PutMapping(value = "/actualizarEstado/{id}/{estado}",consumes = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<Reclamo>actualizarEstadoPorId(@PathVariable("id")Integer idReclamo,
                                                         @PathVariable("estado") String nuevoEstado){
@@ -42,6 +64,14 @@ public class ReclamoController {
         }
     }
 
+    @Operation(summary = "Asignar Personal Municipal al Reclamo")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Personal Municipal asignado al Reclamo",content = {
+                    @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Reclamo.class))
+            }),
+            @ApiResponse(responseCode = "403",description = "Conflicto con el servidor",content = {@Content})
+    })
     @PutMapping(value = "/asignarPersonalMunicipal/{id}",consumes = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<Reclamo>asignarPersonalMunicipal(@PathVariable("id")Integer idReclamo,
                                                            @RequestBody PersonalMunicipal personalMunicipal){
@@ -53,6 +83,14 @@ public class ReclamoController {
         }
     }
 
+    @Operation(summary = "Devuelve un listado de todos los Reclamos")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Listado de Reclamos recuperado",content = {
+                    @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Reclamo.class))
+            }),
+            @ApiResponse(responseCode = "403",description = "Conflicto con el servidor",content = {@Content})
+    })
     @GetMapping(value = "/listar", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<List<Reclamo>>listarReclamos(){
         try {
@@ -63,6 +101,14 @@ public class ReclamoController {
         }
     }
 
+    @Operation(summary = "Buscar Reclamo por ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",description = "Devuelve el Reclamo solicitado",content = {
+                    @Content(mediaType = "application/json",
+                    schema = @Schema(implementation = Reclamo.class))
+            }),
+            @ApiResponse(responseCode = "403",description = "Conflicto con el servidor",content = {@Content})
+    })
     @GetMapping(value = "/buscar/{id}", produces = {MediaType.APPLICATION_JSON_VALUE})
     public ResponseEntity<Reclamo>buscarReclamoPorId(@PathVariable("id")Integer idReclamo){
         try {
