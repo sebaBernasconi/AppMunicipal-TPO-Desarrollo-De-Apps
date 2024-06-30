3c7d5693c0f7d491859fb8f893d8dbcd9a2dcf7f
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-06-30 16:14:03 -0300

    Anotaciones de Swagger agregadas a la clase Denuncia

--- src/main/java/ar/edu/uade/appmunicipal/model/Denuncia.java
+++ src/main/java/ar/edu/uade/appmunicipal/model/Denuncia.java
@@ -1,12 +1,14 @@
 package ar.edu.uade.appmunicipal.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(description = "Denuncia Model")
 @Entity
 @Table(name = "denuncias")
 
@@ -16,30 +18,35 @@ import lombok.Setter;
 @NoArgsConstructor //Constructor vacio para la persistencia
 public class Denuncia {
 
+    @Schema(description = "Id de la Denuncia", requiredMode = Schema.RequiredMode.AUTO)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_denuncia")
     private Integer idDenuncia;
 
+    @Schema(description = "Vecino asociado a la Denuncia", requiredMode = Schema.RequiredMode.REQUIRED)
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "dni")
     private Vecino vecino;
 
+    @Schema(description = "Sitio asociado a la Denuncia", requiredMode = Schema.RequiredMode.REQUIRED)
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "id_sitio")
     private Sitio sitio;
 
+    @Schema(description = "Descripcion de la Denuncia",requiredMode = Schema.RequiredMode.REQUIRED,example = "Ruidos molestos a la madrugada")
     @Column(name = "descripcion")
     private String descripcion;
 
-    //Transformar en enumeracion(?
-    //Patron state?(jodaaaa)
+    @Schema(description = "Estado de la Denuncia", requiredMode = Schema.RequiredMode.AUTO)
     @Column(name = "estado")
     private String estado;
 
+    @Schema(description = "Indica si el Vecino acepta las consecuencias si la Denuncia fuera Falsa",requiredMode = Schema.RequiredMode.REQUIRED,example = "true")
     @Column(name = "acepta_responsabilidad")
     private boolean aceptaResponsabilidad;
 
+    @Schema(description = "Imagenes de la Denuncia",requiredMode = Schema.RequiredMode.REQUIRED)
     @Lob
     @JsonIgnore
     @Column(name = "imagen_local", columnDefinition = "LONGBLOB")
