6e0c0b3859e5f45f20481288e09a43a1c44f8b5e
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-06-30 14:01:11 -0300

    Anotaciones de Swagger agregadas a la clase Rubro

--- src/main/java/ar/edu/uade/appmunicipal/model/Rubro.java
+++ src/main/java/ar/edu/uade/appmunicipal/model/Rubro.java
@@ -1,11 +1,13 @@
 package ar.edu.uade.appmunicipal.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(description = "Rubro Model")
 @Entity
 @Table(name = "rubros")
 
@@ -15,11 +17,13 @@ import lombok.Setter;
 @NoArgsConstructor //Constructor vacio para persistencia
 public class Rubro {
 
+    @Schema(description = "Id del Rubro",requiredMode = Schema.RequiredMode.AUTO)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_rubro")
     private Integer idRubro;
 
+    @Schema(description = "Descripcion del Rubro",requiredMode = Schema.RequiredMode.REQUIRED,example = "Plomero")
     @Column(name = "descripcion")
     private String descripcion;
 
