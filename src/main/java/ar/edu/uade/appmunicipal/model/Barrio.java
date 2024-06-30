8c2d4383338c611cb7b0f044ad9fc1807bf398d3
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-06-30 16:15:21 -0300

    Anotaciones de Swagger agregadas a la clase Barrio

--- src/main/java/ar/edu/uade/appmunicipal/model/Barrio.java
+++ src/main/java/ar/edu/uade/appmunicipal/model/Barrio.java
@@ -1,11 +1,13 @@
 package ar.edu.uade.appmunicipal.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(description = "Barrio Model")
 @Entity
 @Table(name = "barrio")
 
@@ -15,11 +17,13 @@ import lombok.Setter;
 @NoArgsConstructor //Constructor vacio para persistencia
 public class Barrio {
 
+    @Schema(description = "Id del Barrio",requiredMode = Schema.RequiredMode.AUTO )
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "cod_barrio")
     private Integer idBarrio;
 
+    @Schema(description = "Nombre del Barrio",requiredMode = Schema.RequiredMode.REQUIRED,example = "Barrio ingles")
     @Column(name = "nombre")
     private String nombre;
 
