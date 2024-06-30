d9defe1977ea8b70026e7a9e1ee7a77c5a268d6a
Sebastian Bernasconi <136746810+sebaBernasconi@users.noreply.github.com>
2024-06-30 16:09:43 -0300

    Anotaciones de Swagger agregadas a la clase Desperfecto

--- src/main/java/ar/edu/uade/appmunicipal/model/Desperfecto.java
+++ src/main/java/ar/edu/uade/appmunicipal/model/Desperfecto.java
@@ -1,11 +1,13 @@
 package ar.edu.uade.appmunicipal.model;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+@Schema(description = "Desperfecto Model")
 @Entity
 @Table(name = "desperfectos")
 
@@ -15,15 +17,18 @@ import lombok.Setter;
 @NoArgsConstructor //Constructor vacio para la persistencia
 public class Desperfecto {
 
+    @Schema(description = "Id del Desperfecto",requiredMode = Schema.RequiredMode.AUTO)
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_desperfecto")
     private Integer idDesperfecto;
 
+    @Schema(description = "Rubro al que pertenece el desperfecto",requiredMode = Schema.RequiredMode.REQUIRED)
     @ManyToOne(cascade = CascadeType.ALL)
     @JoinColumn(name = "id_rubro")
     private Rubro rubro;
 
+    @Schema(description = "Descripcion del Desperfecto",requiredMode = Schema.RequiredMode.REQUIRED,example = "Las luces del semaforo no andan")
     @Column(name = "descripcion")
     private String descripcion;
 
